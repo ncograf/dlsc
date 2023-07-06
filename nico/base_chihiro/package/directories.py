f5afcf5fb781e25e4dcdaebfa1da878068d73c10
Nico Graf <ncograf@gmail.com>
2023-07-06 14:50:10 +0200

    Extend chihiros solution

---
+++ nico/base_chihiro/package/directories.py
@@ -0,0 +1,16 @@
+import os
+
+
+base_dir = './nico/base_chihiro'
+
+plot_dir = os.path.join(base_dir, 'plots')
+if not os.path.isdir(plot_dir):
+    os.mkdir(plot_dir)
+
+lambda_path = os.path.join(plot_dir, 'lambda.png')
+pde_loss_path = os.path.join(plot_dir, 'pde_loss.png')
+nontriv_loss_path = os.path.join(plot_dir, 'nontriv.png')
+orth_loss_path = os.path.join(plot_dir, 'orth.png')
+rm_path = os.path.join(plot_dir, 'rm.png')
+all_path = os.path.join(plot_dir, 'all.png')
+solutions_path = os.path.join(plot_dir, 'solutions.png')
